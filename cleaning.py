31cb7157af5b1bb91d409b807935d04a50f6205e
Utkarsh Nandy <utkarsh.nandy@gmail.com>
2025-09-09 20:57:23 -0700

    defined standard returns and verified compounding math on toy sample

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-
+import pandas as pd
 
 data = yf.download(
   ["AAPL", "MSFT", "SPY"],
@@ -11,5 +11,19 @@ data = yf.download(
 )
 
 data.drop_duplicates()
-appl_data = data["SPY"]
-print(appl_data)
+
+# we will choose simple returns for P&L
+aapl_sample = data["AAPL"]["Close"]
+aapl_daily_returns = aapl_sample.pct_change()
+
+# verify compounding math works
+
+# manually compound calculation
+initial_val = 100
+compound_val = initial_val * (1 + aapl_daily_returns).prod()
+manual_return = (compound_val - initial_val) / initial_val
+print("Manual compounded return: " , manual_return)
+
+# point to point calculation
+point_to_point = (aapl_sample.iloc[-1] / aapl_sample.iloc[0]) - 1
+print("Point-to-point return:", point_to_point)
